74383ce4576ea9c841d7863bd8adbf08050e167e
Sontome <93179211+Sontome@users.noreply.github.com>
2025-07-10 14:25:10 +0700

    Add files via upload

---
+++ savecookie.py
@@ -0,0 +1,12 @@
+from playwright.sync_api import sync_playwright
+import json
+
+with sync_playwright() as p:
+    browser = p.chromium.connect_over_cdp("http://localhost:9222")
+    context = browser.contexts[0]  # lấy context đầu tiên
+    cookies = context.cookies("https://p2p.tima.vn")
+    
+    with open("tima_cookies.json", "w") as f:
+        json.dump(cookies, f, indent=2)
+
+    print("✅ Đã lưu cookies từ session đang mở!")
